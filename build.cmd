827b9f35d3d864e7b8548b39bcebd1845a56c749
TonyApuzzo <TonyApuzzo@yahoo.com>
2017-10-02 22:48:40 -0600

    Update build.cmd with better comments.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,6 @@
+::
+:: Create the Magisk Module installable Zip archive.
+::
 ECHO off
 
 :: Full path to this script
@@ -6,7 +9,7 @@ SET _mypath=%~dp0
 :: Name of this directory (which is the module name)
 FOR %%G IN (%_mypath:~0,-1%) DO SET _mydir=%%~nG
 
-:: Name of archive to update/create
+:: Name of archive to create
 SET _archive="%_mypath%%_mydir%.zip"
 
 :: Remove the archive since we don't want deleted/renamed files in the archive
