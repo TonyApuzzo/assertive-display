367fb6cef2aed6bf099f38fe9f97db83c226400b
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2017-10-02 22:13:39 -0600

    Comments in build script.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,30 @@
 @ECHO off
+
+:: Full path to this script
 SET _mypath=%~dp0
+
+:: Name of this directory (which is the module name)
 FOR %%G IN (%_mypath:~0,-1%) DO SET _mydir=%%~nG
-7z.exe u -r -ssc -- "%_mypath%%_mydir%.zip" .\common\ .\META-INF\ .\system\ .\config.sh .\module.prop .\README.md
+
+:: Name of archive to update/create
+SET _archive="%_mypath%%_mydir%.zip"
+
+:: Remove the archive since we don't want deleted/renamed files in the archive
+del "%_archive%"
+
+:: Change the working directory
+pushd "%_mypath%"
+
+:: 7zip syntax is ugly
+::     Add to archive
+::     |  Zip format
+::     |  |     Maximum compression
+::     |  |     |     Use Unicode filenames
+::     |  |     |     |       Recursive
+::     |  |     |     |       |  Case sensitive
+::     |  |     |     |       |  |    Exclude files matching *~
+::     |  |     |     |       |  |    |
+7z.exe a -tzip -mx=9 -mcu=on -r -ssc -x!*~ -- "%_archive%" .\common\ .\META-INF\ .\system\ .\config.sh .\module.prop .\README.md
+
+:: Restore the working directory
+popd
