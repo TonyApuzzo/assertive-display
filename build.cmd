28619f724d9204673c166cfc32e4e8046c73c29d
Tony Apuzzo <Tony.Apuzzo@flatironsjouve.com>
2017-10-02 22:13:39 -0600

    Comments in build script.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,21 @@
 @ECHO off
+
+:: Full path to this script
 SET _mypath=%~dp0
+
+:: Name of this directory (which is the module name)
 FOR %%G IN (%_mypath:~0,-1%) DO SET _mydir=%%~nG
-7z.exe u -r -ssc -- "%_mypath%%_mydir%.zip" .\common\ .\META-INF\ .\system\ .\config.sh .\module.prop .\README.md
+
+:: Name of archive to update/create
+SET _archive="%_mypath%%_mydir%.zip"
+
+:: 7zip syntax is ugly
+::     Update archive
+::     |  Zip format
+::     |  |     Maximum compression
+::     |  |     |     Use Unicode filenames
+::     |  |     |     |       Recursive
+::     |  |     |     |       |  Case sensitive
+::     |  |     |     |       |  |    Exclude files matching *~
+::     |  |     |     |       |  |    |
+7z.exe u -tzip -mx=9 -mcu=on -r -ssc -x!*~ -- "%_archive%" .\common\ .\META-INF\ .\system\ .\config.sh .\module.prop .\README.md
